77599612be0baa438a5abce0fb61602bd1b9987c
Rene Zbasnik <rzbasnik@icloud.com>
2023-02-26 13:19:06 +0000

    added dropdown list for choosing stocks and commodities

--- Desktop/Disertation/Website/modules/functions.js
+++ Desktop/Disertation/Website/modules/functions.js
@@ -4,7 +4,7 @@ import { Deplot } from "https://deno.land/x/deplot@0.3.0/mod.ts";
 
 
 
-export async function showForums(){
+export async function getStock(){
     //let sql = `SELECT * FROM apple;`
     const x = `SELECT Date FROM apple;`;
     const y = `SELECT Price FROM apple;`;
@@ -34,3 +34,54 @@ export async function showForums(){
     return datas
 }
 
+export async function getCommodity(){
+    //let sql = `SELECT * FROM apple;`
+    const x = `SELECT Date FROM gold;`;
+    const y = `SELECT Price FROM gold;`;
+    const result1 = await db.query(x)
+    const result2 = await db.query(y)
+    const output1 = [];
+    const output2 = [];
+    result1.forEach( (value) => {
+         return output1.push(value.Date); 
+     });
+    result2.forEach( (value) => {
+        return output2.push(value.Price); 
+    });
+    var trace1 = {
+      x: output1,
+      y: output2,
+      type: 'scatter'
+  };
+  
+  var data = [trace1];
+  
+  var layout = {
+      title: 'Scroll and Zoom',
+      showlegend: true
+  };
+  let datas = {data}
+    return datas
+}
+
+export async function getData(){
+    const u = `show tables;`
+    const tables = await db.query(u)
+    let output = []
+    tables.forEach( (value) => {
+        console.log(value.Tables_in_website)
+        //for(key in value){
+        //    output.push(value[key])    
+        //}
+        //output = {table: value.Tables_in_website};
+        output.push(value.Tables_in_website)
+        //console.log(output)
+        return output
+        //output1.push(value.Date); 
+    });
+    console.log(output)
+    //let out = JSON.stringify(u2)
+    //let tables = {table: u2}
+    //console.log(tables)
+    return output
+}
--- Desktop/Disertation/Website/routes.js
+++ Desktop/Disertation/Website/routes.js
@@ -4,7 +4,7 @@ import { Router } from 'https://deno.land/x/oak@v6.5.1/mod.ts'
 import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts'
 // import { upload } from 'https://cdn.deno.land/oak_upload_middleware/versions/v2/raw/mod.ts'
 // import { parse } from 'https://deno.land/std/flags/mod.ts'
-import { showForums } from './modules/functions.js'
+import { getData, getStock } from './modules/functions.js'
 //import { plot } from "https://deno.land/x/deplot/mod.ts";
 
 const handle = new Handlebars({ defaultLayout: '' })
@@ -14,9 +14,10 @@ const router = new Router()
 router.get('/', async context => {
 	//const authorised = context.cookies.get('authorised')
 	//const data = { authorised }
-	let output = await showForums();
+	let output = await getStock();
+	let tables = await getData();
 	let data = JSON.stringify(output)
-	data = {data: data}
+	data = {data: data, tables}
 	console.log(data)
 	const body = await handle.renderView('home', data)
 	context.response.body = body
